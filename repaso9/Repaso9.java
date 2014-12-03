db8dc41f120a1f36ff060939006f8b189e7b13f3
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-03 21:14:41 +0100

    Creamos el ejercicio 9 de repaso. Carrera de relevos.

---
+++ repaso9/Repaso9.java
@@ -0,0 +1,26 @@
+/*
+ * Escribe una clase llamada Relevos que simule una carrera de relevos de la siguiente forma:
+ * -Cree 4 hilos que se quedarán a la espera de recibir alguna señal para comenzar a correr.
+ *  Una vez creados los hilos, se indicará que comience la carrera, con lo que uno de los hilos
+ *  deberá comenzar a correr. 
+ * -Cuando un hilo termina de correr pone algún mensaje en pantalla y espera un par de 	segundos,
+ *  pasando el testigo a otro de los hilos y comience a correr, y terminando su	ejecución.
+ * -Cuando el último hilo termine de correr, el padre mostrará un mensaje indicando quetodos los hijos han terminado.
+ */
+package repaso9;
+
+/**
+ *
+ * @author findag
+ */
+public class Repaso9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
